ad6ea10c7deb92a721b128412b374d3fb9b08393
vanesaaliu <94717763+vanesaaliu@users.noreply.github.com>
2023-01-08 13:26:30 +0100

    updates

---
+++ bin_parses.py
@@ -0,0 +1,11 @@
+def readBytes(filename):
+bytes = ''
+with open(filename,'rb') as file:
+while True:
+b = file.read(1)
+if not b:
+break
+bytes=bytes+str((int.from_bytes(b, byteorder='big')))
+print(bytes)
+
+readBytes('binary.bin')
