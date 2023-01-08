00e7e9f44955b1e376a3fe99faa916828f05408f
vanesaaliu <94717763+vanesaaliu@users.noreply.github.com>
2023-01-08 16:30:35 +0100

    deleted unused code

--- bin_parses.py
+++ bin_parses.py
@@ -12,15 +12,3 @@ def readBytes(filename):
 
 
 
-
-# def readBytes(filename):
-#     bytes = ''
-#     with open(filename,'rb') as file:
-#     while True:
-#     b = file.read(1)
-# if not b:
-# break
-# bytes=bytes+str((int.from_bytes(b, byteorder='big')))
-# print(bytes)
-
-# readBytes('binary.bin')
