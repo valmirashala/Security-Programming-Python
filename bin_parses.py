d8bfd775e9491a587333ca5edb2cf94aea37eb49
vanesaaliu <94717763+vanesaaliu@users.noreply.github.com>
2023-01-08 13:36:38 +0100

    fixed a few small details

--- bin_parses.py
+++ bin_parses.py
@@ -1,11 +1,26 @@
 def readBytes(filename):
-bytes = ''
-with open(filename,'rb') as file:
-while True:
-b = file.read(1)
-if not b:
-break
-bytes=bytes+str((int.from_bytes(b, byteorder='big')))
-print(bytes)
+    bytes=''
+    with open(filename,'rb') as file:
+        while True:
+            b=file.read(1)
+            if not b:
+                break
+            bytes=bytes+str((int.from_bytes(b,byteorder='big')))
+            print(bytes)
+            readBytes('binary.bin')
 
-readBytes('binary.bin')
+
+
+
+
+# def readBytes(filename):
+#     bytes = ''
+#     with open(filename,'rb') as file:
+#     while True:
+#     b = file.read(1)
+# if not b:
+# break
+# bytes=bytes+str((int.from_bytes(b, byteorder='big')))
+# print(bytes)
+
+# readBytes('binary.bin')
