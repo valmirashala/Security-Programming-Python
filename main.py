97c9bfdf9927efb7c761ba2d93ed7f2d48241aa0
ofertaramadani <oferte.ramadani@student.uni-pr.edu>
2023-01-08 14:44:18 +0100

    adding bin_parses to main

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from elftools.elf.elffile import ELFFile
 import elf_functions
 import magic
+import chardet
+import bin_parses
 
 file=input('Path of the elf file you want to parse:') #a.out ne rastin tone
 with open(file, 'rb') as f:
     magic_number = f.read(4)
+    data = f.read()
+    result = chardet.detect(data)
 
 # Check if the magic number matches the ELF magic number
     if magic_number == b'\x7fELF':
         print('The file is an ELF file.')
         elf_file = ELFFile(f)
         elf_functions.analyze_elf_file(elf_file)
-        
-        
+# Check if the file is a binary string
+    elif result['encoding'] == None:
+        print('The file is an .bin file.')
+        bin_parses.readBytes(file)
     else:
-        # The file is not an ELF file, check what file it is
+        # The file is not an ELF or .bin file, check what file it is
         file_type = magic.from_file(file)
         print(f'The file is not an ELF file. Its type is: {file_type}')
         #TODO: create methods thar parse other binary files
